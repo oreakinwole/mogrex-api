1d36df7b30d6d5266c8b4a52a5055639f42893bf
oreakinwole <oreakinwole@gmail.com>
2025-05-25 13:02:12 +0100

    feat: update routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,27 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
+    Route::middleware('throttle:auth')->group(function () {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+    });
 
 
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('me', [AuthController::class, 'me']);
+
+
+        Route::middleware('throttle:transactions')->group(function () {
+            Route::get('transactions', [TransactionController::class, 'index']);
+            Route::post('transactions', [TransactionController::class, 'store']);
+            Route::get('transactions/{transactionId}', [TransactionController::class, 'show']);
+            Route::get('balance', [TransactionController::class, 'getBalance']);
+        });
+    });
 });
